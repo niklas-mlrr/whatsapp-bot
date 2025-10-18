4ac7cd1cc2ed2d0214591413328299c1863d78f7
niklas.mlrr <niklas.mlrr@gmail.com>
2025-10-18 11:33:55 +0200

    f

--- frontend/vue-project/src/config/websocket.ts
+++ frontend/vue-project/src/config/websocket.ts
@@ -1,11 +1,10 @@
 export const websocketConfig = {
     broadcaster: 'reverb',
     key: 'whatsapp-bot-key',
-    wsHost: '127.0.0.1',
-    wsPort: 8080,
-    wssPort: 8080,
-    forceTLS: false,
+    wsHost: 'lukas-whatsapp.cloud',
+    wsPort: 443,
+    wssPort: 443,
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
     disableStats: true,
 } as const;
-
